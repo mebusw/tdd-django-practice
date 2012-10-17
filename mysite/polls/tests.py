70e918dba1fc39fa86a7948605a0f8f6edf1d1bc
mebusw <mebusw@163.com>
2012-10-17 16:32:18 +0800

    Relations between models: Polls and Choices, Further customisations of the admin view: related objects inline

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -7,7 +7,7 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from django.utils import timezone
-from polls.models import Poll
+from polls.models import Poll, Choice
 
 class PollModelTest(TestCase):
     def test_creating_a_new_poll_and_saving_it_to_the_database(self):
@@ -38,3 +38,42 @@ class PollModelTest(TestCase):
         p = Poll()
         p.question = 'How is babby formed?'
         self.assertEquals(unicode(p), 'How is babby formed?')                
+        
+class ChoiceModelTest(TestCase):
+
+    def test_creating_some_choices_for_a_poll(self):
+        # start by creating a new Poll object
+        poll = Poll()
+        poll.question="What's up?"
+        poll.pub_date = timezone.now()
+        poll.save()
+
+        # now create a Choice object
+        choice = Choice()
+
+        # link it with our Poll
+        choice.poll = poll
+
+        # give it some text
+        choice.choice = "doin' fine..."
+
+        # and let's say it's had some votes
+        choice.votes = 3
+
+        # save it
+        choice.save()
+
+        # try retrieving it from the database, using the poll object's reverse
+        # lookup
+        poll_choices = poll.choice_set.all()
+        self.assertEquals(poll_choices.count(), 1)
+
+        # finally, check its attributes have been saved
+        choice_from_db = poll_choices[0]
+        self.assertEquals(choice_from_db, choice)
+        self.assertEquals(choice_from_db.choice, "doin' fine...")
+        self.assertEquals(choice_from_db.votes, 3)
+
+    def test_choice_defaults(self):
+        choice = Choice()
+        self.assertEquals(choice.votes, 0)        
